157b9915a586486658603c6323de0d0d03907181
Og Maciel <omaciel@rpath.com>
2010-04-10 08:59:36 -0400

    Added new translator context into builtins.

--- src/lib/i18n.py
+++ src/lib/i18n.py
@@ -12,8 +12,19 @@ try:
     from gettext import gettext as _, ngettext
     gettext.install(program, unicode=True)
     gettext.textdomain(program)
+
+    def C_(ctx, s):
+        """Provide qualified translatable strings via context.
+            Taken from gnome-games.
+        """
+        translated = gettext.gettext('%s\x04%s' % (ctx, s))
+        if '\x04' in translated:
+            # no translation found, return input string
+            return s
+        return translated
     import __builtin__
     __builtin__.__dict__['ngettext'] = ngettext
+    __builtin__.__dict__['C_'] = C_
 except ImportError:
     import sys
     print >> sys.stderr, ("You don't have gettext module, no " \
@@ -21,13 +32,3 @@ except ImportError:
     import __builtin__
     __builtin__.__dict__['_'] = lambda x: x
     __builtin__.__dict__['ngettext'] = lambda x, y, n: (n == 1) and x or y
-
-def C_(ctx, s):
-    """Provide qualified translatable strings via context.
-        Taken from gnome-games.
-    """
-    translated = gettext.gettext('%s\x04%s' % (ctx, s))
-    if '\x04' in translated:
-        # no translation found, return input string
-        return s
-    return translated
