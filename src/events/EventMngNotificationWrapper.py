0edb0609235309b2f8ae443c296835700fb18c55
Carlos Ribeiro <carlosmribeiro1@gmail.com>
2011-11-24 01:41:27 +0000

    Added a new notification wrapper

---
+++ src/events/EventMngNotificationWrapper.py
@@ -0,0 +1,31 @@
+##########################################################################
+# Copyright 2009 Carlos Ribeiro
+#
+# This file is part of Radio Tray
+#
+# Radio Tray is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 1 of the License, or
+# (at your option) any later version.
+#
+# Radio Tray is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radio Tray.  If not, see <http://www.gnu.org/licenses/>.
+#
+##########################################################################
+
+from events import EventManager
+
+class EventMngNotificationWrapper:
+
+    def __init__(self, eventManager):
+        self.eventManager = eventManager
+
+
+
+    def notify(self, title, message):
+        self.eventManager.notify(self.eventManager.NOTIFICATION, {'title': title, 'message':message})
