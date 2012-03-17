9f7e76687cadffefdc647c119948be880761000c
Valdur Kana <valdur55@gmail.com>
2012-03-17 20:35:46 +0200

    Added radiotray-connecting, copy more icons to pixmaps folder, better Indentation

--- src/AppIndicatorGui.py
+++ src/AppIndicatorGui.py
@@ -141,11 +141,11 @@ class AppIndicatorGui:
             # not available in this version of app indicator
             self.log.info("App indicator scroll events are not available.")
 
-        def app_indicator_scroll(self, indicator, delta, direction):
-            if direction == 0:
-                self.mediator.volume_up()
-            else:
-                self.mediator.volume_down()
+    def app_indicator_scroll(self, indicator, delta, direction):
+        if direction == 0:
+            self.mediator.volume_up()
+        else:
+            self.mediator.volume_down()
 
 
     def update_radios(self):
